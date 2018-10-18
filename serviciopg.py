c1ae693b93ca793a7f737e7c019e81a1bfe113ef
Carlos Delgado <carlos_96_delgado@hotmail.com>
2018-10-18 14:05:15 -0500

    fa

--- serviciopg.py
+++ serviciopg.py
@@ -10,37 +10,37 @@ def conectar():
     try:
         with open('C:/ws/config.yaml','r') as ymlfile:
             cfg = yaml.load(ymlfile)
-			   
+
         host = cfg['database']['host']
         base = cfg['database']['base']
         port = cfg['database']['port']
         user = cfg['database']['user']
         pswd = cfg['database']['passwd']
-		
+
         conn = pgsql(host, base, port, user, pswd)
-        return conn		
+        return conn
     except yaml.YAMLError as e:
         print("error")
         return None
 
-		
+
 @route('/lectura')
 def lectura():
     response.headers['Content-Type'] = 'application/json'
     conn=conectar()
-		
+
     query = "SELECT texto AS texto,encode(imagen,'base64') AS imagen FROM prueba"
     data = conn.getData(query)
-                
+
     platos = []
-    
+
     for valor in data:
         platos.append({'imagen': valor.imagen, 'texto': valor.texto})
-		
+
     conn.cerrar()
     return json.dumps(platos)
-    
+
 
 app = application = bottle.default_app()
 if __name__ == "__main__":
-    bottle.run(host='181.198.202.181', port=8095)
+    bottle.run(host='192.168.0.120', port=8095)
